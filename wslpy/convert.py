490b39b3eb63d73126362295f3f955cd6e0c23df
Logan Campos <logan.campos1998@gmail.com>
2021-06-03 04:35:46 -0500

    WIP draft for seamless path detection/conversion

--- wslpy/convert.py
+++ wslpy/convert.py
@@ -2,9 +2,60 @@
 
 This is the class that helps convert between 3 types of path used widely in WSL.
 """
+from os import PathLike
 import re
 from enum import Enum
-from .exceptions import *
+from typing import Union
+from .exceptions import (
+    INVALID_CONVERSION_TYPE,
+)
+from abc import abstractmethod
+from pathlib import (
+    Path, PosixPath, WindowsPath,
+)
+import asyncio
+
+@abstractmethod
+def coro_is_linux_path():
+    ...
+
+@abstractmethod
+def coro_is_windows_path():
+    ...
+
+@abstractmethod
+async def _to():
+    ...
+
+@abstractmethod
+async def _main(path: str or Path):
+    loop = asyncio.get_event_loop()
+    (_posix, _windows) = loop.create_future()
+    _posix = loop.create_task(coro_is_linux_path)
+    _windows = loop.create_task(coro_is_windows_path)
+    __ = asyncio.gather(_posix, _windows)
+    ...
+    
+class _AbstractPath_T(PathLike):
+    '''generic path type to use in abstract base class.'''
+    __DoubleWindowsPath_T: Union[Path, WindowsPath]
+    __slots__ = (
+        'AUTO',
+        'LINUX',
+        'WIN',
+        'WINDOUBLE',
+    )
+    AUTO: 'Path'
+    LINUX: 'PosixPath'
+    WINDOWS: 'WindowsPath'
+    DOUBLE_WINDOWS: '__DoubleWindowsPath_T'
+    ...
+    def __repr__(self: PathLike) -> Union[type, Path]:
+        ...
+    ...
+    def __init__(self) -> None:
+        self.path_type: super.__repr__(Path(self))
+        super(Path).__init__(self)        
 
 class PathConvType(Enum):
     """Types for Path Conversions
@@ -26,7 +77,6 @@ class PathConvType(Enum):
     WIN = 2
     WINDOUBLE = 3
 
-
 def __Lin2Win__(path):
     # replace / to \
     path = re.sub(r'/', r'\\', path)
@@ -34,10 +84,31 @@ def __Lin2Win__(path):
     path = re.sub(r'\\mnt\\([A-Za-z])', r'\1:', path)
     return path
 
+@abstractmethod
+def linux2windows(path: PosixPath) -> WindowsPath:
+    '''abstract method to convert linux path into a windows path.'''
+    # replace '/' with '\'
+    __: Path = re.sub(r'/', r'\\', path)
+    # replace '\mnt\<drive_letter>' with '<drive_letter>:'
+    __path: WindowsPath = re.sub(r'\\mnt\\[A-Za-z])', r'\1:', __)
+    ...
+    path = __path
+    ...
+    return path
 
 def __Win2Dwin__(path):
     # replace \ to \\
     return re.sub(r'\\', r'\\\\', path)
+
+@abstractmethod
+def windows2double_windows(path: WindowsPath) -> Path:
+    '''abstract method to convert windows path into a double windows path.'''
+    # replace '\' with '\\'
+    _path: Path = re.sub(r'\\', r'\\\\', path)
+    ...
+    path = _path
+    ...
+    return path
 
 
 def __DWin2Lin__(path):
@@ -47,6 +118,17 @@ def __DWin2Lin__(path):
     path = re.sub(r'([A-Za-z]):', r'/mnt/\1', path)
     return path
 
+@abstractmethod
+def double_windows2linux(path: Path) -> PosixPath:
+    '''abstract method to convert double_windows path into a linux path.'''
+    # replace '\\' with '/'
+    __: Path = re.sub('\\\\', r'/', path)
+    # replace '<drive_letter>:' to '\mnt\<drive_letter>'
+    _path: PosixPath = re.sub(r'([A-Za-z]):', r'/mnt/\1', __)
+    ...
+    path = _path
+    ...
+    return path
 
 def to(input: str, toType=PathConvType.AUTO):
     """
